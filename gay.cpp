a5565b028842f36d14528b3d7cb0d7337e3d2c2d
N0tPasquy <106989517+N0tPasquy@users.noreply.github.com>
2024-10-01 23:27:30 +0200

    Some adjustment

--- gay.cpp
+++ gay.cpp
@@ -2,6 +2,8 @@
 // Created by Nazzaro on 01/10/2024.
 //
 #include <iostream>
+using namespace std;
+
 void main(){
-    cout<<"coglione";
+    cout<<"Prova per far capire cosa succede quando viene modificato un file"<<endl;
 }
---
+++ help.cpp
@@ -0,0 +1,11 @@
+//
+// Created by supre on 01/10/2024.
+//
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Prova per far capire cosa succede quando vengono creati altri file"<<endl;
+
+    return 0;
+}
