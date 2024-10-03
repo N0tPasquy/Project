ace9b4bc06e9619e1551ac61746413be97bd7303
N0tPasquy <106989517+N0tPasquy@users.noreply.github.com>
2024-10-03 19:50:31 +0200

    update

--- Rispetta-i-versi/header.hpp
+++ Rispetta-i-versi/header.hpp
@@ -22,9 +22,10 @@ using namespace std;
 
 class Risolutore{
 private:
-    string Problema = "Problema.txt";
-    string Soluzione = "Soluzione.txt";
+string Problema = "Problema.txt";
+string Soluzione = "Soluzione.txt";
 public:
+<<<<<<< Updated upstream
     void Risolvi(){
         fstream F;                            //ifstream classe che ho importato da <fstream>, F è un oggetto della classe ifstream
         //F.open("Soluzione.txt");             //F è un oggetto di ifstream, quindi apre in input il file "Soluzione.txt"
@@ -56,8 +57,39 @@ public:
         }
 
         F.close();                             //dopo tutte le operazioni chido il file
+=======
+void Risolvi(){
+    ifstream F;                            //ifstream classe che ho importato da <fstream>, F è un oggetto della classe ifstream
+    //F.open("Soluzione.txt");             //F è un oggetto di ifstream, quindi apre in input il file "Soluzione.txt"
+    F.open(Soluzione.c_str());             // .c_str() estrae la stringa di caratteri in stile C
+>>>>>>> Stashed changes
 
+    if(!F){
+        cerr << "Impossibile aprire il file"<<endl;     //con questo if controllo se il file è stato aperto correttamente, se così non fosse lo notifico a video ed esco
+        exit(0);
     }
+
+    F.seekg(ios::beg);                  //imposto il puntatore del file all'inizio del file, nel caso l'ultima apertura non lo avesse reimpostato
+
+    string Numeri, Simboli, Rigo;       //dichiaro le stringhe e le variabili di appoggio
+    char ch;
+
+    while (!F.eof()) {
+        getline(F, Rigo);
+        //!-- DA FINIRE --
+        //!-- AIUTO NON SO COSA FARE ODIO LE STRINGHE VAFFANCULO --
+        //!-- NON SO NEMMENO SE STAVO ANDANDO BENE, SPERO DI SI --
+        //!-- NEL NOME DEL PADRE, DEL FIGLIO E DELLO SPIRITO SANTO --
+        //!-- da capire in modo più chiaro cosa bisgona fare in questo ciclo
+        //!-- dovrei leggere da file UNA SOLA RIGA ALLA VOLTA, salvare i numeri e i caratteri nelle rispettive stringhe create apposta, confrontare la stringa caratteri appena creata con UNA RIGA ALLA VOLTA le righe del file "Problema.txt"
+        cout<<Rigo<<endl;
+        //quando il codice viene eseguito, riusciamo a leggere da file, ora bisogna capire come fare per separare i caratteri '<' e '>'
+        //dai numeri per ogni riga. Qui mi arrendo per ora
+    }
+
+    F.close();                             //dopo tutte le operazioni chido il file
+
+}
 };
 
 #endif //RISPETTA_I_VERSI_HEADER_HPP
