b82298e018c7596a96bb45f1bb51d62ae13a2666
N0tPasquy <106989517+N0tPasquy@users.noreply.github.com>
2024-10-04 17:31:04 +0200

    Update
    Risolto il problema dove non si riusciva a salvare correttamente la stringa Rigo in un array di char. Il problema è che per poter salvare una stringa class-Style (C++) in un array di char (C style) bisogna usare la funzione strcpy(). Questa funzione in input accetta solamente array di char, bisogna quindi passargli Rigo.c_str() in modo da fare la conversione automatica. ora che abbiamo l'array di char ch contenente il rigo del file procediamo a separare i numeri dai simboli < e >

--- Rispetta-i-versi/header.hpp
+++ Rispetta-i-versi/header.hpp
@@ -16,8 +16,13 @@
 #define RISPETTA_I_VERSI_HEADER_HPP
 
 #include <iostream>
-#include <string>
-#include <fstream>
+#include <string>   //questa libreria invece lavora sulle stringhe trattandole come classi, quindi in modo più dinamico e in stile C++.
+
+                    //con questa libreria possiamo lavorare sulle stringhe come se fossero array di char, come si fa in C,
+#include <cstring>  //si utilizza per le sue funzioni più importanti come: strcpy(), strcmp(). La prima fa la copia di una stringa in un altra,
+                    //la seconda confronta due stringhe
+
+#include <fstream>  //libreria importata per poter utilizzare gli oggetti di tipo fstream, ifstream e ofstream. Serve per leggere/scrivere su file
 using namespace std;
 
 class Risolutore {
@@ -37,30 +42,38 @@ public:
             exit(0);
         }
 
-        F.seekg(ios::beg);                  //imposto il puntatore del file all'inizio del file, nel caso l'ultima apertura non lo avesse reimpostato
-
-        string Numeri, Simboli, Rigo;       //dichiaro le stringhe e le variabili di appoggio
+        //TUTTA QUESTA PARTE, FINO ALLA RIGA 50, NON È NECESSARIA. VIENE USATA SOLO PER FARE DELLE PROVE DI DEBUG
+        string Rigo;            //dichiaro la stringa in stile C++ e le variabili che ci serviranno
         getline(F, Rigo);
-        //F.seekg(ios::beg);
+        F.seekg(ios::beg);                  //imposto il puntatore del file all'inizio del file, nel caso l'ultima apertura non lo avesse reimpostato
+        int Lunghezza = Rigo.size()+1;
+        char ch[Lunghezza];
 
         while (!F.eof()) {
-            int Lunghezza = Rigo.size()+1;
-            char ch[Lunghezza];
-            F.get(ch, Lunghezza);
+            //--> Procedo a salvare un rigo del file letto in input in una stringa classe stile C++
+            getline(F, Rigo);   //con getline() mi salvo la riga del file nella variabile Rigo, nella prima iterazione contiene: 1<4>2<3<5
 
-            cout<<ch<<endl;
+            //--> adesso bisogna convertire string Rigo in un array di caratteri
+            strcpy(ch, Rigo.c_str());   //bisogna includere la libreria <cstring> per usare questa function. Non fa altro che copiare una stringa dentro un altra stringa.
+                                                    // In input accetta solo array di char, per questo la stringa "Rigo" utilizzo il metodo .c_str(), non fa altro che convertire
+                                                    // da tipo classe a tipo array di char
 
-            getline(F, Rigo);
+            //--> Una volta copiata la stringa C++ Rigo nell' array di char C ch, possiamo procedere a separare i numeri dai simboli '<' e '>'
+
+            /*Lunghezza = Rigo.size()+1;
+            for (int i = 0; i < Lunghezza; i++) {
+                    cout<<ch[i];
+            }
+            cout<<endl;*/
 
             //!-- DA FINIRE --
             //!-- AIUTO NON SO COSA FARE ODIO LE STRINGHE VAFFANCULO --
-            //!-- NON SO NEMMENO SE STAVO ANDANDO BENE, SPERO DI SI --coglione
+            //!-- NON SO NEMMENO SE STAVO ANDANDO BENE, SPERO DI SI --
             //!-- NEL NOME DEL PADRE, DEL FIGLIO E DELLO SPIRITO SANTO --
             //!-- da capire in modo più chiaro cosa bisgona fare in questo ciclo
-            //!-- dovrei leggere da file UNA SOLA RIGA ALLA VOLTA, salvare i numeri e i caratteri nelle rispettive stringhe create apposta, confrontare la stringa caratteri appena creata con UNA RIGA ALLA VOLTA le righe del file "Problema.txt"
-            //cout << Rigo << endl;
-            //quando il codice viene eseguito, riusciamo a leggere da file, ora bisogna capire come fare per separare i caratteri '<' e '>'
-            //dai numeri per ogni riga. Qui mi arrendo per ora
+            //!-- dovrei leggere da file UNA SOLA RIGA ALLA VOLTA, salvare i numeri e
+            //! i caratteri nelle rispettive stringhe create apposta, confrontare la stringa
+            //! caratteri appena creata con UNA RIGA ALLA VOLTA le righe del file "Problema.txt"
         }
 
         F.close();                             //dopo tutte le operazioni chido il file
