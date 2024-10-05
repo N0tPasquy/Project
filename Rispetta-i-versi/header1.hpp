700f773f008c87cc5b7a73da04a35944518d1d0e
N0tPasquy <106989517+N0tPasquy@users.noreply.github.com>
2024-10-05 15:00:28 +0200

    Update - added comments
    il codice è lo stesso, ho commentato tutto quello già scritto in precedenza in modo che sia più chiaro il funzionamento di ogni metodo

--- Rispetta-i-versi/header1.hpp
+++ Rispetta-i-versi/header1.hpp
@@ -19,39 +19,42 @@ using namespace std;
 
 class esito{
 private:
-    string soluzione;
+    string soluzione;   //variabili dove salvo il contenuto di ogni rigo che viene passato in input
     string problema;
 public:
-    esito(string sol, string prob): soluzione(sol), problema(prob){};
+    esito(string sol, string prob): soluzione(sol), problema(prob){};   //Per ogni istanza di esito, prendo in input l'
+                                                                        // i-esimo rigo che viene passato dall main
 
-    string scomponiSimboli(string &sol){
+    string scomponiSimboli(string &sol){    //metodo per dividere i numeri dai simboli < e >
         string simboli;
-        for (int i = 0; i < sol.length(); i++) {
-            if(sol[i] == '<' || sol[i] == '>'){
-                simboli += sol[i];
-                sol.erase(i, 1);
+        for (int i = 0; i < sol.length(); i++) {    //ciclo for per scorrere tutto il rigo attuale di soluzione
+            if(sol[i] == '<' || sol[i] == '>'){ //controllo l' i-esimo carattere di soluzione
+                simboli += sol[i];      //se in posizione [i] di "sol" trovo uno tra i simboli < e > lo salvo nella stringa "simboli"
+                sol.erase(i, 1);    //una volta salvato il simbolo, lo elimino dalla stringa di partenza
+            }                              //così facendo alla fine del ciclo nella stringa "sol" (che sarebbe il rigo di Soluzione.txt)
+        }                                  //troveremo solamente numeri
+        return simboli; //ritorno la nuova stringa contente i simboli < e > del rigo Soluzione.txt,
+    }                   // mentre peri numeri lavoro direttamente sulla stringa originale tramite riferimento
+
+    bool confrontaSimboli(string *prob, string *simboli){   //metodo dove vengono confrontati i simboli trovati in precedenza con i simboli di "Problema.txt", prendo in input i puntatori alle stringhe
+        if(prob->length() != simboli->length())     //verifico che entrambi i righi abbiano la stessa lunghezza
+            return false;   // se così non fosse significa che non è soluzione, quindi ritorno falso
+
+        for(int i = 0; i < prob->length(); i++){    //ciclo che scorre il rigo di problema
+            if(prob[i] != simboli[i]){  //se l' i-esimo simbolo di "prob" e l' i-esimo simbolo di "simboli" sono
+                return false;           //diversi significa che non è soluzione, quindi ritorno falso
             }
         }
-        return simboli;
+        return true; //nel caso sia tutto uguale ritorno vero
     }
 
-    bool confrontaSimboli(string *prob, string *simboli){
-        for(int i = 0; i < prob->length(); i++){
-            if(prob[i] != simboli[i]){
-                return false;
-            }
+    bool verificaNumeri(string simb, string &num){  //metodo per controllare che in ogni rigo ci siano N numeri quante sono le "caselle" di Soluzione.txt
+        for (int i = 0; i < simb.length()+1; i++) { //le caselle sono sempre il numero dei simboli + 1, quindi itero il ciclo tante volte quante sono le caselle
+            if((num[i] - '0') > simb.length()+1){   //Con num[i] - '0' viene eseguita la sottrazione ASCII tra l' i-esimo num e 0, questo permette la conversione da string a int,
+                return false;                       // dopo la conversione verifico se il numero appena trovato è piu grande del numero massimo consentito
+            }                                       // se così fosse significa che non è soluzione, quindi ritorno falso
         }
-        return true;
-    }
-
-    bool verificaNumeri(string simb, string &num){
-        cout<<simb.length()+1<<endl;
-        for (int i = 0; i < simb.length()+1; i++) {
-            if((num[i] - '0') > simb.length()+1){   //Con num[i] - '0' viene eseguita la sottrazione ASCII tra l' i-esimo num e 0,
-                return false;                       // questo permette la conversione da string a int
-            }
-        }
-        return true;
+        return true;    //nel caso in cui tutti i numeri non superano N (dove N è il numero di caselle) ritorno vero
     }
 
 };
--- Rispetta-i-versi/main.cpp
+++ Rispetta-i-versi/main.cpp
@@ -14,6 +14,12 @@ int main() {
         return 0;
     }
 
+    //!-- Il codice di seguito funziona solamente per il primo rigo, bisogna fare un ciclo while
+    //! con condizione "fin quando non si arriva alla fine dei file", cioè while(S.eof() && P.eof())
+    //! e all' interno del while ripetere il codice che segue, va bene anche se viene copiato e incollato
+    //! dovrebbe andare lo stesso siccome non andiamo mai a modificare il seekg() di entrambi i file,
+    //! ma viene aggiornato solamente quando leggiamo le varie righe all' inizio del codice
+
     string rigoSol, rigoProb, simboli;
     getline(S, rigoSol);
     getline(P, rigoProb);
@@ -22,14 +28,17 @@ int main() {
     cout<<simboli<<endl;
     cout<<rigoSol<<endl;
     if(a.confrontaSimboli(&rigoProb, &simboli)){
-        cout<<"I simboli non combaciano"<<endl; //invece di questo queto cout deve esserci il modo per scrivere su file
+        cout<<"I simboli non combaciano"<<endl; //invece di questo queto cout deve esserci il modo per scrivere sul file Esito.txt "Sbagliata"
     }else{
         cout<<"I simboli combaciano"<<endl;
-        bool prova = a.verificaNumeri(rigoProb,rigoSol);
-        if(prova)
-            cout<<"vero"<<endl;
+        if(a.verificaNumeri(rigoProb,rigoSol))
+            cout<<"vero"<<endl; //! Arrivati a questo punto: abbiamo verificato che i simboli combacino,
+                                //! abbiamo verificato che i numeri siano giusti, dobbiamo continuare e fare tutti i confronti tra i numeri
+                                //! consiglio di fare prima questo metodo e solo una volta finito di continuare con la scrittura sul
+                                //! file "Esito.txt", bisogna verificare se esiste già e poi aggiungere in "APPEND" l' esito.
+                                //! Se il file "Esito.txt" non esiste nel primo caso, bisogna crearlo
         else
-            cout<<"falso"<<endl;
+            cout<<"falso"<<endl;    //Invevece di questo cout deve esserci il modo per scrivere sul file Esito.txt "Sbagliata"
     }
 
 
