600e5fb3d026d37da48519dd202a74419681cc78
Pasquale Pagano <106989517+N0tPasquy@users.noreply.github.com>
2024-10-08 16:14:14 +0200

    V0.1 (#3)
    * First version
    
    Prima versione del codice dell'esercizio di Laboratorio di ASD Lezione 2.
    Questa è solo una "Inizializzazione", cioè è solo il codice di partenza:
    apro il file Soluzione.txt, leggo tutte le righe.
    
    * Small fix
    
    Small fix in general for a better reading expirience
    
    * small changes
    
    * Update ReadMe.txt
    
    * update
    
    ho messo 2 righe
    
    * small changes
    
    intentation changes
    
    * update
    
    * tolta la monnezza
    
    * sDAFDASD (proca)
    
    * update
    
    piccolissimi passi avanti, ma ancora non abbiamo concluso nulla
    
    * Update
    
    Risolto il problema dove non si riusciva a salvare correttamente la stringa Rigo in un array di char. Il problema è che per poter salvare una stringa class-Style (C++) in un array di char (C style) bisogna usare la funzione strcpy(). Questa funzione in input accetta solamente array di char, bisogna quindi passargli Rigo.c_str() in modo da fare la conversione automatica. ora che abbiamo l'array di char ch contenente il rigo del file procediamo a separare i numeri dai simboli < e >
    
    * Better update
    
    Il codice precedente utilizzava gli array di char, ho riscritto il codice utilizzando i vector e cosi facendo ho semplificato la divisione dei numeri dai simboli < e >
    
    * Update
    
    Scritto il codice fino alla parte dove divido i numeri dai simboli '<' e '>'
    
    * Update
    
    Ho scritto solo 2 righe di codice in più per ricordarmi cosa dovessi continuare a fare
    
    * Update header.hpp
    
    * NEW HEADER
    
    Creato un nuovo header siccome avevo sbagliato ad interpretare la traccia, ogni coppia di righi va visualizzata come ISTANZA DI UNA CLASSE e non come stavo procedendo prima, tutto nella stessa classe.
    Nel nuovo header c'è ancora poco e va ancora rivisto per una corretta gestione delle stringhe e dei vector
    
    * Update
    
    Versione aggiornata del codice, siamo andati avanti solo che non ci sono ancora i commenti
    !-- TO DO --!
    aggiugni i commenti
    
    * Update - added comments
    
    il codice è lo stesso, ho commentato tutto quello già scritto in precedenza in modo che sia più chiaro il funzionamento di ogni metodo
    
    * Finito il file header.hpp
    
    Il file header.hpp è completo, tutti possibili casi per rigo sono stati controllati, ricontrollati e stracontrollati mokk a chi te viv.
    ORA BISOGNA:
    lavoare al main - nulla di complicato, solamente:
    -> fare una verifica se entrambi i file hanno lo stesso numero di righi;
    -> crare un ciclo while che continua ad andare fin quando non finisce uno dei due file (se hanno lo stesso numero di righi)
    -> giustamente creare un array di oggetti RIGO, così nel while ogni iterazione lavorerà sull' i-esimo rigo di ogni file
    
    * Update - almost done
    
    Finito gran parte dell' esercizio, manca solamente l'ultima modifica al main.
    -> nel file main.cpp al posto di tutti i cout presenti nel ciclo while bisogna far scrivere su file, non dovrebbe essere complicato.
    Dopo quest utlima modifica l'esercizio dovrebbe essere finito
    
    * Update header.hpp
    
    dimenticato di eliminare 2 metodi inutili dal file header.hpp
    
    * Completed at 95%
    
    L' esercizio è FINITO.
    L' unica modifica importante DA FARE è che questi algoritmi lavorino sugli attributi di classe rigo, dobbiamo fare in modo che invece lavorino sugli attributi 'problema' e 'soluzione' della classe "rigo"
    
    * Update
    
    ultimo push prima di finire
    BISOGNA
    sistemare i commenti nel file header.hpp
    aggiungerne un paio nel file main.cpp
    
    * pulito il cazzo di codice
    
    * LAST PUSH
    
    esercizio completo sotto ogni aspetto, pronto per il merge con il main
    
    ---------
    
    Co-authored-by: Miqweaschi <nazzarotessitore48@gmail.com>

---
+++ Rispetta-i-versi/header.hpp
@@ -0,0 +1,87 @@
+//
+// Created by Pasquale & Nazzaro il patatone on 05/10/2024.
+//
+
+#ifndef RISPETTA_I_VERSI_HEADER_HPP
+#define RISPETTA_I_VERSI_HEADER_HPP
+
+#include <string>
+#include <cstring>
+using namespace std;
+
+class rigo{
+private:
+    string soluzione;   //variabili dove salvo il contenuto di ogni rigo che viene passato in input
+    string problema;
+    string simboli;
+public:
+    rigo(): soluzione(" "), problema(" "){};
+
+    void setSoluzione(string sol){
+        soluzione = sol;
+    }
+
+    void setProblema(string prob){
+        problema = prob;
+    }
+
+
+    void scomponiSimboli(){                                //! Estrae i simboli < e > da una stringa (soluzione) e li salva in un’altra stringa (simboli),
+        for (int i = 0; i < soluzione.length(); i++) {      //! eliminandoli dalla stringa originale.
+            if(soluzione[i] == '<' || soluzione[i] == '>'){
+                simboli += soluzione[i];
+                soluzione.erase(i, 1);
+            }
+        }
+    }
+
+    bool confrontaSimboli(){                          //!confronta i simboli estratti dalla soluzione con quelli nel problema. Se c'è una
+        if(problema.length() != simboli.length())      //! differenza, restituisce true (non è una soluzione); altrimenti, false (è una soluzione).
+            return true;
+        for(int i = 0; i <= problema.length(); i++){
+            if(problema[i] != simboli[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool controllaDuplicati(){                        //!Verifica se ci sono numeri duplicati nella soluzione utilizzando un array di booleani per
+        bool Char[256] = {false};                    //!tracciare i caratteri visti. Restituisce true se non ci sono duplicati, false altrimenti.
+        for(int i = 0; i < soluzione.length(); i++){
+            char ch = soluzione[i];
+            if(Char[ch]){
+                return false;
+            }
+            Char[ch] = true;
+        }
+        return true;
+    }
+
+    bool verificaNumeri(){                               //!Controlla che i numeri nella soluzione non superino un certo limite (pari al numero di
+        for (int i = 0; i < simboli.length()+1; i++) {      //!simboli + 1). Restituisce true se valido, false altrimenti.
+            if((soluzione[i] - '0') > simboli.length()+1){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool verificaUguaglianze(){                            //!Verifica che i numeri nella soluzione rispettino le disuguaglianze rappresentate dai simboli
+        for(int i = 0; i < simboli.length(); i++){             //!< e >. Restituisce true se tutte le condizioni sono rispettate, false altrimenti.
+            if(simboli[i] == '<'){
+                if((soluzione[i] - '0') > (soluzione[i+1] - '0')){
+                    return false;
+                }
+            }else{
+                if((soluzione[i] - '0') < (soluzione[i+1] - '0')){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+};
+
+#endif //RISPETTA_I_VERSI_HEADER_HPP
--- Rispetta-i-versi/main.cpp
+++ Rispetta-i-versi/main.cpp
@@ -1,6 +1,98 @@
+#include "header.hpp"
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ifstream P("Problema.txt"), S("Soluzione.txt"); //apro in input i due file da leggere
+    ofstream E("Esito.txt");    //apro in output il file su cui bisogna scrivere l' esito
+
+    if(!E){ //controllo che ogni file venga aperto in modo corretto
+        ofstream E("Esito.txt");
+        if(!E){
+            cerr << "Impossibile aprire ii file 'Problema.txt'"<< endl;     //Con questo if controllo se il file è stato aperto correttamente, se così non fosse notifico a video ed esco dal programma
+            return 0;
+        }
+    }
+
+    if(!S){   //Controllo se uno dei due file non è stato aperto correttamente
+        cerr << "Impossibile aprire ii file 'Soluzione.txt'"<< endl;     //Con questo if controllo se il file è stato aperto correttamente, se così non fosse notifico a video ed esco dal programma
+        return 0;
+    }
+
+    if(!P){
+        cerr << "Impossibile aprire ii file 'Problema.txt'"<< endl;     //Con questo if controllo se il file è stato aperto correttamente, se così non fosse notifico a video ed esco dal programma
+        return 0;
+    }
+
+    int nRigheSol = 0, nRigheProb = 0;  //variabili di conteggio dei righi di ogni file
+    string tmp;
+
+
+    while(getline(S, tmp)){
+        nRigheSol++;    //conto il numero di righi nel file soluzione
+    }
+
+    while(getline(P, tmp)){
+        nRigheProb++;   //conto il numero di righi nel file problema
+    }
+
+    if(nRigheProb != nRigheSol){
+        E<<"Il file soluzione non e' compatibile con il problema"<<endl;    //se non dovessero avere lo stesso numero di righi non posso continuare con lo svolgimento
+        return 0;
+    }
+
+    int dim = nRigheSol, i = 0; //contatori interi per il ciclo while
+    rigo A[nRigheSol];  //creo l' array di oggetti di tipo rigo, per ogni posizione dell' array corrisponde un rigo del problema e un rigo della soluzione
+
+    //metodi che reimpostano il puntatore del file all'inizio
+    P.clear();
+    S.clear();
+    E.clear();
+    P.seekg(ios::beg);
+    S.seekg(ios::beg);
+    E.seekp(ios::beg);
+
+    while(dim > 0){ //il ciclo viene ripetuto "dim" volte, dove dim sono il numero di righi dei file
+        //codice che va ripetuto per ogni rigo
+        string rigoSol, rigoProb, simboli;
+
+        getline(S, rigoSol);
+        getline(P, rigoProb);
+
+        A[i].setSoluzione(rigoSol);
+        A[i].setProblema(rigoProb);
+        A[i].scomponiSimboli();
+
+
+        if(A[i].confrontaSimboli()){    // Ritorna vero se i simboli sono diversi
+            E<<"Sbagliata"<<endl;
+        }else{
+
+            if(A[i].controllaDuplicati()){  // Ritorna vero se non ci sono duplicati
+
+                if(A[i].verificaNumeri()) { // Ritorna vero se i numeri all' interno di soluzione soddisfano la traccia
+
+                    if(A[i].verificaUguaglianze()){ //Ritorna vero se tutte le uguaglianze di soluzione sono verificate
+                        E<<"Corretta"<<endl;
+                    }else{
+                        E<<"Sbagliata"<<endl;
+                    }
+                }else {
+                    E << "Sbagliata" << endl;
+                }
+            }else{
+                E<<"Sbagliata"<<endl;
+            }
+        }
+        dim--;
+        i++;
+    }
+
+    S.close();  //alla fine del ciclo while chiudo tutti i file aperti in precedenza
+    P.close();
+    E.close();
+
     return 0;
 }
