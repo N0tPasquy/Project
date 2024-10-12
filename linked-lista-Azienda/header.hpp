799ac9d2fbbe7442466fba68981fc1e48807a4d7
N0tPasquy <106989517+N0tPasquy@users.noreply.github.com>
2024-10-12 14:20:31 +0200

    Update header.hpp

--- linked-lista-Azienda/header.hpp
+++ linked-lista-Azienda/header.hpp
@@ -1,6 +1,10 @@
 //
 // Created by Pasquale on 12/10/2024.
 //
+//Progettare e implementare una lista (linked list) di
+//impiegati di un'azienda. Un impiegato ha un nome ed un
+//salario. Un manager è un impiegato cui viene corrisposto
+//un bonus oltre al salario.
 
 #ifndef LINKED_LISTA_AZIENDA_HEADER_HPP
 #define LINKED_LISTA_AZIENDA_HEADER_HPP
