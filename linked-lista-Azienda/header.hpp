a0ec3fdd171d836499183b967267cbe73b84b5c3
N0tPasquy <106989517+N0tPasquy@users.noreply.github.com>
2024-10-12 17:29:19 +0200

    update

--- linked-lista-Azienda/header.hpp
+++ linked-lista-Azienda/header.hpp
@@ -9,66 +9,11 @@
 #ifndef LINKED_LISTA_AZIENDA_HEADER_HPP
 #define LINKED_LISTA_AZIENDA_HEADER_HPP
 
+#include <iostream>
 #include <string>
 
 using namespace std;
 
-
-class impiegato{
-private:
-    string nome;
-    float salario;
-public:
-    impiegato(){
-        nome = "";
-        salario = 0;
-    }
-
-    void setNome(string n){
-        nome = n;
-    }
-
-    void setSalario(float s){
-        salario = s;
-    }
-
-    string getNome(){
-        return nome;
-    }
-
-    float getSalario(){
-        return salario;
-    }
-};
-
-class manager : public impiegato{
-private:
-    float bonus;
-public:
-    manager() : impiegato(){
-        bonus = 0;
-    }
-
-    void setBonus(float b){
-        bonus = b;
-    }
-
-    float getBonus(){
-        return bonus;
-    }
-};
-
-class azienda{
-private:
-    //! Creare una lista di impiegati e stamparla a video sfruttando il principio del polimorfismo
-    //! Implementare l' overload dell'operatore + in modo da sommare i salari di tutti gli impiegati.
-    impiegato a;
-    azienda* next{};
-public:
-    azienda(){
-
-        next = nullptr;
-    };
-};
+//da rifare completamente
 
 #endif //LINKED_LISTA_AZIENDA_HEADER_HPP
