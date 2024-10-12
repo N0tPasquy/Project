9a64950f0961a3cf37614dd6813ee5af9b7bf0a6
N0tPasquy <106989517+N0tPasquy@users.noreply.github.com>
2024-10-12 10:24:59 +0200

    inizializzata cartella di lavoro

---
+++ linked-lista-Azienda/header.hpp
@@ -0,0 +1,21 @@
+//
+// Created by Pasquale on 12/10/2024.
+//
+
+#ifndef LINKED_LISTA_AZIENDA_HEADER_HPP
+#define LINKED_LISTA_AZIENDA_HEADER_HPP
+
+#include <string>
+
+using namespace std;
+
+
+class impiegato{
+private:
+    string nome;
+    float salario;
+public:
+    //costruttore di default o parametrizzato e metodi set e get
+};
+
+#endif //LINKED_LISTA_AZIENDA_HEADER_HPP
