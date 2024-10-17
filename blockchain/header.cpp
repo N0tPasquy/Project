37d6058025aa23ffe3742de506045e66e52ec31b
N0tPasquy <106989517+N0tPasquy@users.noreply.github.com>
2024-10-17 18:10:19 +0200

    fisrt commit

---
+++ blockchain/header.cpp
@@ -0,0 +1,38 @@
+//
+// Created by Pasquale on 17/10/2024.
+//
+#include "header.hpp"
+
+transizione::transizione(std::string f, std::string t, int q) {
+    from = f;
+    to = t;
+    qt = q;
+}
+
+void transizione::setFrom(std::string f) {
+    from = f;
+}
+
+string transizione::getFrom() {
+    return from;
+}
+
+void transizione::setTo(std::string t) {
+    to = t;
+}
+
+string transizione::getTo() {
+    return to;
+}
+
+void transizione::setQt(int q) {
+    qt = q;
+}
+
+int transizione::getQt() {
+    return qt;
+}
+
+blocco::blocco(T* tmp, ){
+    
+}
--- blockchain/header.hpp
+++ blockchain/header.hpp
@@ -7,11 +7,39 @@
 
 #include <iostream>
 #include <string>
+using namespace std;
+
+class transizione{
+private:
+    string from;
+    string to;
+    int qt;
+public:
+    transizione(string, string, int);
+
+    void setFrom(string);
+    string getFrom();
+
+    void setTo(string);
+    string getTo();
+
+    void setQt(int);
+    int getQt();
+};
 
 template <class T>
-T somma(T a, T b){
-    T sum = a + b;
-    return sum;
-}
+class blocco{
+private:
+    T* contenuto;
+    blocco *next;
+public:
+    blocco(T*, blocco*);
+
+    void setContenuto(T*);
+    T* getContenuto();
+
+    void setNext(blocco*);
+    blocco* getNext();
+};
 
 #endif //BLOCKCHAIN_HEADER_HPP
