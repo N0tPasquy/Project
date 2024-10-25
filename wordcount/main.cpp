8eea2a50fceeb7c6f54e34a1061d84a4e1c3ba6c
N0tPasquy <106989517+N0tPasquy@users.noreply.github.com>
2024-10-25 08:59:57 +0200

    Aggiunti commenti al codice
    in più creato un nuovo file di input per fare altri test

--- wordcount/main.cpp
+++ wordcount/main.cpp
@@ -7,35 +7,37 @@
 
 #include <iostream>
 #include <string>
-#include <fstream>
-#include <map>
+#include <fstream>  //libreria per i file
+#include <map>  //libreria per usare le map
 using namespace std;
 
 int main() {
-    string line;
+    string line;    //stringa di appoggio
 
-    map<string, int> occorrenze;
-    map<string, int>::iterator it;
+    map<string, int> occorrenze;    //map occorrenze
 
-    ifstream input("Input.txt");
+    ifstream input("Input1.txt");   //scegliere il file "Input.txt" per fare la verifica su un testo più grande
+
     if(!input){
         cerr<<"File non aperto correttamente"<<endl;
         return 0;
     }
 
-    input.seekg(ios::beg);
-    while (!input.eof()){
-        getline(input, line, ' ');
-        while (!line.empty() && (line.back() == ',' || line.back() == '.'))
-            line.pop_back();
-        occorrenze[line]++;
+    //Il programma è case sensitive
+    while (!input.eof()){   //ripete fin quando non finisce il file
+        getline(input, line, ' ');  //ritorna ogni parola prima di uno spazio
+        while (!line.empty() && (line.back() == ',' || line.back() == '.'))     //controlla che alla fine di ogni parola non ci sia un punto o una virgola
+            line.pop_back();    //se ci fosse eseguendo pop_back() rimuove l' ultimo elemento in coda, nel nostro caso la virgola o il punto
+        occorrenze[line]++; //incrementa il numero di occorrenze della parola trovata, se non c'è nella map, automaticamente crea una nuova posizione con key uguale alla parola mancante
     }
 
-    input.close();
+    input.close();  //chiudo il file aperto in precedenza
 
-    for(auto& pair : occorrenze){
-        cout<<pair.first<<": "<<pair.second<<endl;
-    }
+    for(auto& a: occorrenze){    //ciclo ranged-for, ripete il ciclo per tutta la dimensione di occorrenze, auto trova in automatico il tipo di variabile da associare ad "a", nel nostro caso sarà di tipo pair
+        cout<<a.first<<": "<<a.second<<endl;  //stampa a video sia key (la parola del testo) che il numero di occorrenze
+    }   //a.first ritorna il valore key della map, a.second ritorna il numero int salvato in map
+        //Viene utilizzato il tipo pair siccome la mappa necessita proprio di due elementi e salviamo ogni record di "map<string, int> occorrenze" in "pair<string, int> a"
+        //Per ogni iterazione di ciclo il pair punterà al successivo di map partendo dal primo elemento in ordine alfabetico, perché la key è una stringa e viene gestita con l'ordine lessico-grafico
 
     return 0;
 }
